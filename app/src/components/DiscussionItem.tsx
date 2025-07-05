6e2f829b148999bdeb67ef43547f3f19cc3bd926
Arup Jana <arupjana7365@gmail.com>
2025-07-05 20:53:52 +0530

    temp: added discussions page code

---
+++ app/src/components/DiscussionItem.tsx
@@ -0,0 +1,9 @@
+const DiscussionItem = () => {
+    return (
+        <div className="discussion-item">
+        <h2>Discussion Title</h2>
+        <p>This is a brief description of the discussion topic.</p>
+        <button className="join-button">Join Discussion</button>
+        </div>
+    );
+}
