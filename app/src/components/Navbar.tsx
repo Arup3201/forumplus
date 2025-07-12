cdf9f92e29ef4f1bbcbd81cf9b0ffb85d3be85d5
Arup3201 <arupjana7365@gmail.com>
2025-07-12 23:50:09 +0530

    update: desktop profile button

--- app/src/components/Navbar.tsx
+++ app/src/components/Navbar.tsx
@@ -147,7 +147,11 @@ export const Navbar = () => {
                 Account
               </DropdownMenuItem>
               <DropdownMenuItem>
-                <BadgeIcon colorClass="bg-red-500" className="has-[>svg]:p-0" value="2">
+                <BadgeIcon
+                  colorClass="bg-red-500"
+                  className="has-[>svg]:p-0"
+                  value="2"
+                >
                   <Icons.Bell />
                 </BadgeIcon>
                 Notifications
@@ -193,9 +197,58 @@ export const Navbar = () => {
         <BadgeIcon value="4+" colorClass="bg-red-500">
           <Icons.Bell size={24} />
         </BadgeIcon>
-        <Button variant="ghost">
-          <Icons.User size={24} />
-        </Button>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost">
+              {user ? (
+                <OptimizedAvatar
+                  src={user.avatarUrl}
+                  alt={user.displayName}
+                  fallbackText={user.displayName?.split(" ")[0][0] || "User"}
+                  className="w-[24px] h-[24px]"
+                />
+              ) : (
+                <Icons.User size={24} />
+              )}
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end" sideOffset={20}>
+            <DropdownMenuLabel className="p-0 font-normal">
+              <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                {user ? (
+                  <OptimizedAvatar
+                    src={user.avatarUrl}
+                    alt={user.displayName}
+                    fallbackText={user.displayName?.split(" ")[0][0]}
+                    className="w-10 h-10"
+                  />
+                ) : (
+                  <Icons.User size={24} />
+                )}
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate font-medium">
+                    {user?.displayName}
+                  </span>
+                  <span className="text-muted-foreground truncate text-xs">
+                    {user?.email}
+                  </span>
+                </div>
+              </div>
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              <DropdownMenuItem>
+                <Icons.UserCircle />
+                Account
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <Icons.Logout />
+              Log out
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
         <div className="flex items-center space-x-2">
           <Label htmlFor="dark-mode" className="text-gray-700 md:hidden">
             Dark Mode
