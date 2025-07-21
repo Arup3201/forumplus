913419c9c72b128cde48aa1494a6b21e4d575453
Arup3201 <arupjana7365@gmail.com>
2025-07-21 20:03:07 +0530

    fix: searchbox input not showing

--- app/src/components/search-box.tsx
+++ app/src/components/search-box.tsx
@@ -1,18 +1,30 @@
-import { cn } from '@/lib/utils'
-import type { SeachboxProps } from '@/types/components/searchbox'
-import { Icons } from "./icons"
-import { Input } from "./ui/input"
+import type { SeachboxProps } from "@/types/components/searchbox";
+import { Icons } from "./icons";
+import { Input } from "./ui/input";
+import { cn, generateId } from "@/lib/utils";
 
 const Searchbox = ({
-    size,
-    placeholder = "Search",
-    className = "",
-    onChange = () => {}
+  size,
+  placeholder = "Search",
+  className = "",
+  onChange = () => {},
 }: SeachboxProps) => {
-    return <div className={cn("flex items-center gap-1 px-2 py-0 border-2 border-gray-200 rounded-4xl", className)}>
-        <Icons.Search size={size} className="text-gray-400" />
-        <Input className="bg-transparent shadow-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0 w-full" placeholder={placeholder} onChange={onChange} />
+  return (
+    <div
+      className={cn(
+        "flex items-center gap-1 px-2 py-0 border-2 border-gray-200 rounded-sm w-full",
+        className,
+      )}
+    >
+      <Icons.Search size={size} className="text-gray-400" />
+      <Input
+        id={generateId("search-input-id-")}
+        className="bg-transparent shadow-transparent border-0 focus-visible:ring-0 focus-visible:ring-offset-0"
+        placeholder={placeholder}
+        onChange={onChange}
+      />
     </div>
-}
+  );
+};
 
 export default Searchbox;
