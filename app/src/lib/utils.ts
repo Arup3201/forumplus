74b168d2d410e2f741a51e0fbb579e09b3b72102
Arup3201 <arupjana7365@gmail.com>
2025-07-21 19:38:13 +0530

    refactor: prettier and eslint added

--- app/src/lib/utils.ts
+++ app/src/lib/utils.ts
@@ -1,6 +1,12 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function generateId(prefix = "id") {
+  return (
+    prefix + Date.now().toString(36) + Math.random().toString(36).substr(2)
+  );
 }
